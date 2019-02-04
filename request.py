4c9dd332e7ff3aa985b6b805e805bb99da19b1e1
jotasprout <jotasprout@gmail.com>
2019-02-04 10:29:37 -0500

    more variables and comments

--- request.py
+++ request.py
@@ -1,30 +1,13 @@
-# LastFM variables
-LastFM_baseURL = 'http://ws.audioscrobbler.com/2.0/?method='
+# Get MBID for artist
+# By going to MusicBrainz.org and search
+# Refer to blog post on next line for instructions
+# https://jotascript.wordpress.com
 
-# Part of URL for getting LastFM artist info
-LastFM_artistInfo = 'artist.getinfo&mbid='
 
-LastFM_artistMBID = ''
 
-# Part of URL for getting LastFM album info
-LastFM_albumInfo = 'album.getinfo&mbid='
-
-LastFM_albumMBID = ''
-
-# LastFM API key
-LastFM_apiKey = '&api_key=333a292213e03c10f38269019b5f3985'
-
-# LastFM response format
-LastFM_jsonFormat = '&format=json'
-
-# Total LastFM URL
-LastFM_totalURL = LastFM_baseURL + LastFM_artistInfo + LastFM_MBID + LastFM_apiKey + LastFM_jsonFormat
-
-# Other LastFM
-LastFM_artistListeners = ''
-LastFM_artistPlaycount = ''
-LastFM_albumListeners = ''
-LastFM_albumPlaycount = ''
+# Loop through Releases array 
+# For each Release, get MBID for recordings on that release
+# Store MBID for each Recording in an array
 
 # MusicBrainz variables
 MusicBrainz_baseURL = 'https://www.musicbrainz.org/ws/2/'
@@ -42,6 +25,10 @@ MusicBrainz_releasegroupMBID = ''
 # Part of URL for getting MusicBrainz Release Groups info
 MusicBrainz_releaseGroups = '?inc=release-groups'
 
+# Store MBID for each Release-Group in an array
+# Loop through Release-Group array
+# For each Release-Group, get MBID for releases in that group
+
 # Part of URL for getting MusicBrainz Releases info
 MusicBrainz_releases = '?inc=releases'
 
@@ -52,6 +39,9 @@ MusicBrainz_releaseMBID = ''
 
 # Part of URL for getting MusicBrainz Recordings info
 MusicBrainz_recordings = '?inc=recordings'
+
+# Recording MBID isn't used at MusicBrainz but at LastFM
+MusicBrainz_recordingMBID = ''
 
 # MusicBrainz response format
 MusicBrainz_jsonFormat = '&fmt=json'
@@ -65,17 +55,60 @@ releaseGroups_totalURL = MusicBrainz_baseURL + MusicBrainz_artistMethod + MusicB
 # Get Releases for a Release-Group from MusicBrainz
 releases_totalURL = MusicBrainz_baseURL + MusicBrainz_releasegroupMethod + MusicBrainz_MBID + MusicBrainz_releaseGroups + MusicBrainz_apiKey + jsonFormat
 
-# Get MBID for artist
-# Store MBID for each Release-Group in an array
-# Loop through Release-Group array
-# For each Release-Group, get MBID for releases in that group
 # Store MBID for each release in an array
-# Loop through Releases array 
-# For each Release, get MBID for recordings on that release
-# Store MBID for each Recording in an array
+
+# Loop through each release in the release array
+
+# For each Release, get MBID for all Recordings and store them in an array
+
+
+
+# LastFM variables
+LastFM_baseURL = 'http://ws.audioscrobbler.com/2.0/?method='
+
+# Part of URL for getting LastFM artist info
+LastFM_artistInfo = 'artist.getinfo&mbid='
+
+LastFM_artistMBID = ''
+
+# Part of URL for getting LastFM album info
+LastFM_albumInfo = 'album.getinfo&mbid='
+
+LastFM_albumMBID = '' # item in array of MusicBrainz_releaseMBID 
+
+# Part of URL for getting LastFM album info
+LastFM_trackInfo = 'track.getinfo&mbid='
+
+LastFM_trackMBID = '' # item in array of MusicBrainz_recordingMBID 
+
+# LastFM API key
+LastFM_apiKey = '&api_key=333a292213e03c10f38269019b5f3985'
+
+# LastFM response format
+LastFM_jsonFormat = '&format=json'
+
+# Total LastFM URL
+LastFM_totalURL = LastFM_baseURL + LastFM_artistInfo + LastFM_MBID + LastFM_apiKey + LastFM_jsonFormat
+
+# Other LastFM
+LastFM_artistListeners = ''
+LastFM_artistPlaycount = ''
+LastFM_albumListeners = ''
+LastFM_albumPlaycount = ''
+LastFM_trackListeners = ''
+LastFM_trackPlaycount = ''
+
+# Get artist Listeners and Playcount from LastFM
+
+# Get Listeners and Playcount for each Album (using Release MBID) by an Artist
+
+# Get Listeners and Playcount for each Track (using Recording MBID) on an Album
 
 f = open ('artist.json', 'a') # a is for append if artist dict already started
 f.write ('variable goes here for data from MusicBrainz')
 f.close()
+
+# Questions to ask 
+## Which artists, albums, tracks, have a lower listener-to-play ratio?
 
 response = requests.get('https://en.wikipedia.org/wiki/Nobel_Prize')
