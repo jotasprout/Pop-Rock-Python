ebef2d538a046e545bd874691e26ebce734897f6
jotasprout <jotasprout@gmail.com>
2019-02-05 10:26:51 -0500

    Organize functions and comments and add more of them

--- request.py
+++ request.py
@@ -1,9 +1,12 @@
+import requests
+
 # Get MBID for artist
 # By going to MusicBrainz.org and search
 # Refer to blog post on next line for instructions
 # https://jotascript.wordpress.com
 
-
+aliceCooperPerson = 'ee58c59f-8e7f-4430-b8ca-236c4d3745ae'
+aliceCooperBand = '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd'
 
 # Loop through Releases array 
 # For each Release, get MBID for recordings on that release
@@ -15,30 +18,46 @@ MusicBrainz_baseURL = 'https://www.musicbrainz.org/ws/2/'
 # Part of URL for using artist MBID
 MusicBrainz_artistMethod = 'artist/'
 
-MusicBrainz_artistMBID = ''
-
-# Part of URL for using Release Groups MBID
-MusicBrainz_releasegroupMethod = 'release-group/'
-
-MusicBrainz_releasegroupMBID = ''
+MusicBrainz_artistMBID = aliceCooperPerson
 
 # Part of URL for getting MusicBrainz Release Groups info
 MusicBrainz_releaseGroups = '?inc=release-groups'
 
+# Get Release-Groups for artist from MusicBrainz
+releaseGroups_totalURL = MusicBrainz_baseURL + MusicBrainz_artistMethod + MusicBrainz_MBID + MusicBrainz_releaseGroups + MusicBrainz_apiKey + jsonFormat
+
 # Store MBID for each Release-Group in an array
+
+MusicBrainz_releasegroupMBID = ''
+
 # Loop through Release-Group array
+
+
 # For each Release-Group, get MBID for releases in that group
+# Part of URL for using Release Groups MBID to get Releases
+MusicBrainz_releasegroupMethod = 'release-group/'
 
 # Part of URL for getting MusicBrainz Releases info
 MusicBrainz_releases = '?inc=releases'
+
+# Get Releases for a Release-Group from MusicBrainz
+releases_totalURL = MusicBrainz_baseURL + MusicBrainz_releasegroupMethod + MusicBrainz_MBID + MusicBrainz_releaseGroups + MusicBrainz_apiKey + jsonFormat
+
+# Store MBID for each release in an array
+
+# Loop through each release in the release array
 
 # Part of URL for using Release MBID
 MusicBrainz_releaseMethod = 'release/'
 
 MusicBrainz_releaseMBID = ''
 
+# For each Release, get MBID for all Recordings and store them in an array
+
 # Part of URL for getting MusicBrainz Recordings info
 MusicBrainz_recordings = '?inc=recordings'
+
+# Store MBID for Recordings in an array
 
 # Recording MBID isn't used at MusicBrainz but at LastFM
 MusicBrainz_recordingMBID = ''
@@ -49,19 +68,13 @@ MusicBrainz_jsonFormat = '&fmt=json'
 # Total MusicBrainz URL
 MusicBrainz_totalURL = MusicBrainz_baseURL + MusicBrainz_artistMethod + MusicBrainz_MBID + MusicBrainz_releaseGroups + MusicBrainz_apiKey + jsonFormat
 
-# Get Release-Groups for artist from MusicBrainz
-releaseGroups_totalURL = MusicBrainz_baseURL + MusicBrainz_artistMethod + MusicBrainz_MBID + MusicBrainz_releaseGroups + MusicBrainz_apiKey + jsonFormat
-
-# Get Releases for a Release-Group from MusicBrainz
-releases_totalURL = MusicBrainz_baseURL + MusicBrainz_releasegroupMethod + MusicBrainz_MBID + MusicBrainz_releaseGroups + MusicBrainz_apiKey + jsonFormat
-
-# Store MBID for each release in an array
-
-# Loop through each release in the release array
-
-# For each Release, get MBID for all Recordings and store them in an array
 
 
+
+
+
+
+response = requests.get('')
 
 # LastFM variables
 LastFM_baseURL = 'http://ws.audioscrobbler.com/2.0/?method='
@@ -69,7 +82,7 @@ LastFM_baseURL = 'http://ws.audioscrobbler.com/2.0/?method='
 # Part of URL for getting LastFM artist info
 LastFM_artistInfo = 'artist.getinfo&mbid='
 
-LastFM_artistMBID = ''
+LastFM_artistMBID = MusicBrainz_artistMBID
 
 # Part of URL for getting LastFM album info
 LastFM_albumInfo = 'album.getinfo&mbid='
