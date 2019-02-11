8353375e2f6845feb9deb5a5455a90ff7cecd8b9
jotasprout <jotasprout@gmail.com>
2019-02-10 19:47:17 -0500

    list of first artists

---
+++ rawFrom_LastFM_MusicBrainz/artistMBIDs.py
@@ -0,0 +1,27 @@
+aliceCooperPerson = 'ee58c59f-8e7f-4430-b8ca-236c4d3745ae'
+aliceCooperBand = '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd'
+
+blackSabbath = '5182c1d9-c7d2-4dad-afa0-ccfeada921a8'
+
+defLeppard = '7249b899-8db8-43e7-9e6e-22f1e736024e'
+
+dio = 'c55193fb-f5d2-4839-a263-4c044fca1456'
+
+elf = '57e0e9f3-24b5-46a6-be00-be793ca26e21'
+
+frost = '6ab0acff-51de-45db-95cc-bdf5a6dd8578'
+
+heavenHell = '484a1d40-0fb9-4768-acff-b570cedaacb4'
+
+kidRock = 'ad0ecd8b-805e-406e-82cb-5b00c3a3a29e'
+
+meatLoaf = 'b134d1bf-c7c7-4427-93ac-9fdbc2b59ef1'
+stoneyMeatLoaf = '2cb3b264-277f-4d8f-bc86-1923ff8abdc0'
+
+ozzyOsbourne = '8aa5b65a-5b3c-4029-92bf-47a544356934'
+
+quietRiot = '5c6acb91-4b9b-4245-b92f-e817295c4ed0'
+
+rainbow = 'e3cb4543-210f-499a-b0d1-3882c312dfb9'
+
+dickWagner = 'f92d6bfd-76e7-4394-aaec-9490756eb50c'
