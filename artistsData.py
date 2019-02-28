ab74215c4a6a15cd2e1062943e4167a72f5b5da5
jotasprout <jotasprout@gmail.com>
2019-02-28 18:14:46 -0500

    fixed array and new data

--- artistsData.py
+++ artistsData.py
@@ -1,6 +1,5 @@
 # MBIDs from MusicBrainz.org
-
-mbid_array = [
+mbid_array2 = [
     '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd', # Alice Cooper band    
     'ee58c59f-8e7f-4430-b8ca-236c4d3745ae', # Alice Cooper person    
     '1a03f20c-26dd-4c26-bbe8-426e05ea46d5', # Amboy Dukes
@@ -13,7 +12,6 @@ mbid_array = [
     '57e0e9f3-24b5-46a6-be00-be793ca26e21', # Elf
     '66bf7876-3898-47fa-8bdd-dc200f946cec', # Evil Stig
     '484a1d40-0fb9-4768-acff-b570cedaacb4', # Heaven and Hell
-    'd30a1d2b-e88d-4470-89cb-69d8c335ce3d', # Hollywood Vampires
     'f376828a-b438-4fda-bb2e-dcd5fbe81f83', # Joan Jett
     '46e63d3b-d91b-4791-bb73-e9f638a45ea0', # Joan Jett and the Blackhearts  
     'b134d1bf-c7c7-4427-93ac-9fdbc2b59ef1', # Meat Loaf
@@ -27,6 +25,61 @@ mbid_array = [
     'ded0d67a-eb71-4fe9-83c0-1d18b8ed497e', # Runaways
     'bbd80354-597e-4d53-94e4-92b3a7cb8f2c' # Saxon    
     ]
+
+mbid_array = [
+    '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd', # Alice Cooper band 
+    'f376828a-b438-4fda-bb2e-dcd5fbe81f83', # Joan Jett
+    '46e63d3b-d91b-4791-bb73-e9f638a45ea0', # Joan Jett and the Blackhearts  
+    'b134d1bf-c7c7-4427-93ac-9fdbc2b59ef1', # Meat Loaf
+    '2cb3b264-277f-4d8f-bc86-1923ff8abdc0', # Stoney and Meat Loaf
+    '26f07661-e115-471d-a930-206f5c89d17c', # Motley Crue
+    'e491fae8-3a5a-438e-8368-925753fb41a1', # Ted Nugent
+    '8aa5b65a-5b3c-4029-92bf-47a544356934', # Ozzy Osbourne    
+    '0383dadf-2a4e-4d10-a46a-e9e041da8eb3', # Queen
+    '5c6acb91-4b9b-4245-b92f-e817295c4ed0', # Quiet Riot
+    'e3cb4543-210f-499a-b0d1-3882c312dfb9', # Rainbow
+    'ded0d67a-eb71-4fe9-83c0-1d18b8ed497e', # Runaways
+    'bbd80354-597e-4d53-94e4-92b3a7cb8f2c' # Saxon    
+    ]
+
+amboydukes_Spot = '5cVLuEqb7aOHuzwssXHzWI'
+anvil_Spot = ''
+blackSabbath_Spot = '5M52tdBnJaKSvOpJGz8mfZ'
+aliceCooper_Spot = '3EhbVgyfGd7HkpsagwL9GS'
+dio_Spot = '4CYeVo5iZbtYGBN4Isc3n6'
+elf_Spot = '3RYdggbT5C9r4BsljokJ1Q'
+evilStig_Spot = ''
+HeavenHell_Spot = '4UjiBRkTw9VmvDZiJZKPJ7'
+hollywoodVampires_Spot = ''
+joanJett_Spot = ''
+meatLoaf_Spot = ''
+metallica_Spot = '2ye2Wgw4gimLv2eAKyk1NB'
+motleyCrue_Spot = '0cc6vw3VN8YlIcvr1v7tBL'
+tednugent_Spot = '21ysNsPzHdqYN2fQ75ZswG'
+ozzyOsbourne_Spot = '6ZLTlhejhndI4Rh53vYhrY'
+queen_Spot = '1dfeR4HaWDbWqFHLkxsg1d'
+quietRiot_Spot = ''
+rainbow_Spot = '6SLAMfhOi7UJI0fMztaK0m'
+runaways_Spot = ''
+saxon_Spot = ''
+
+
+
+_Spot = ''
+_Spot = ''
+_Spot = ''
+_Spot = ''
+_Spot = ''
+_Spot = ''
+_Spot = ''
+_Spot = ''
+_Spot = ''
+
+
+
+
+
+
 
 aliceCooperPerson = 'ee58c59f-8e7f-4430-b8ca-236c4d3745ae'
 hollywoodVampires = 'd30a1d2b-e88d-4470-89cb-69d8c335ce3d'
