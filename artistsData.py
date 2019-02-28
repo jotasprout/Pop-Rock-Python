d228bebe7bee3b986961e37cf7eaaa0e46d57ff6
jotasprout <jotasprout@gmail.com>
2019-02-28 11:16:48 -0500

    Commented and renamed mbid array

--- artistsData.py
+++ artistsData.py
@@ -1,9 +1,32 @@
 # MBIDs from MusicBrainz.org
 
-mbid_array = ['ee58c59f-8e7f-4430-b8ca-236c4d3745ae', '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd', '07a85e96-bb72-4930-b41d-24853f4a4ede', 'b134d1bf-c7c7-4427-93ac-9fdbc2b59ef1', '2cb3b264-277f-4d8f-bc86-1923ff8abdc0', 'e491fae8-3a5a-438e-8368-925753fb41a1', '1a03f20c-26dd-4c26-bbe8-426e05ea46d5', '5182c1d9-c7d2-4dad-afa0-ccfeada921a8', 'c55193fb-f5d2-4839-a263-4c044fca1456', '883871a1-f154-4df8-a7f7-558ea456dd0a', '30f9591a-778b-40dd-be8f-105589f9c998', '57e0e9f3-24b5-46a6-be00-be793ca26e21', 'e3cb4543-210f-499a-b0d1-3882c312dfb9', '8aa5b65a-5b3c-4029-92bf-47a544356934', 'bbd80354-597e-4d53-94e4-92b3a7cb8f2c', '66bf7876-3898-47fa-8bdd-dc200f946cec', '484a1d40-0fb9-4768-acff-b570cedaacb4']
-
-# USE THIS ONE
-mbid_array2 = ['e491fae8-3a5a-438e-8368-925753fb41a1', '1a03f20c-26dd-4c26-bbe8-426e05ea46d5', '5182c1d9-c7d2-4dad-afa0-ccfeada921a8', 'c55193fb-f5d2-4839-a263-4c044fca1456', '883871a1-f154-4df8-a7f7-558ea456dd0a', '30f9591a-778b-40dd-be8f-105589f9c998', '57e0e9f3-24b5-46a6-be00-be793ca26e21', 'e3cb4543-210f-499a-b0d1-3882c312dfb9', '8aa5b65a-5b3c-4029-92bf-47a544356934', 'bbd80354-597e-4d53-94e4-92b3a7cb8f2c', '66bf7876-3898-47fa-8bdd-dc200f946cec', '484a1d40-0fb9-4768-acff-b570cedaacb4', '26f07661-e115-471d-a930-206f5c89d17c', '0383dadf-2a4e-4d10-a46a-e9e041da8eb3', '07a85e96-bb72-4930-b41d-24853f4a4ede', 'f376828a-b438-4fda-bb2e-dcd5fbe81f83', '46e63d3b-d91b-4791-bb73-e9f638a45ea0', 'ded0d67a-eb71-4fe9-83c0-1d18b8ed497e', 'b134d1bf-c7c7-4427-93ac-9fdbc2b59ef1', '2cb3b264-277f-4d8f-bc86-1923ff8abdc0', 'ee58c59f-8e7f-4430-b8ca-236c4d3745ae', '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd']
+mbid_array = [
+    '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd', # Alice Cooper band    
+    'ee58c59f-8e7f-4430-b8ca-236c4d3745ae', # Alice Cooper person    
+    '1a03f20c-26dd-4c26-bbe8-426e05ea46d5', # Amboy Dukes
+    '07a85e96-bb72-4930-b41d-24853f4a4ede', # Anvil
+    '5182c1d9-c7d2-4dad-afa0-ccfeada921a8', # Black Sabbath
+    'c55193fb-f5d2-4839-a263-4c044fca1456', # Dio
+    '9f6c4063-ce0a-4b71-a7b7-a32c91997260', # Dio and the Prophets
+    '883871a1-f154-4df8-a7f7-558ea456dd0a', # Dio and the Redcaps    
+    '30f9591a-778b-40dd-be8f-105589f9c998', # Electric Elves
+    '57e0e9f3-24b5-46a6-be00-be793ca26e21', # Elf
+    '66bf7876-3898-47fa-8bdd-dc200f946cec', # Evil Stig
+    '484a1d40-0fb9-4768-acff-b570cedaacb4', # Heaven and Hell
+    'd30a1d2b-e88d-4470-89cb-69d8c335ce3d', # Hollywood Vampires
+    'f376828a-b438-4fda-bb2e-dcd5fbe81f83', # Joan Jett
+    '46e63d3b-d91b-4791-bb73-e9f638a45ea0', # Joan Jett and the Blackhearts  
+    'b134d1bf-c7c7-4427-93ac-9fdbc2b59ef1', # Meat Loaf
+    '2cb3b264-277f-4d8f-bc86-1923ff8abdc0', # Stoney and Meat Loaf
+    '26f07661-e115-471d-a930-206f5c89d17c', # Motley Crue
+    'e491fae8-3a5a-438e-8368-925753fb41a1', # Ted Nugent
+    '8aa5b65a-5b3c-4029-92bf-47a544356934', # Ozzy Osbourne    
+    '0383dadf-2a4e-4d10-a46a-e9e041da8eb3', # Queen
+    '5c6acb91-4b9b-4245-b92f-e817295c4ed0', # Quiet Riot
+    'e3cb4543-210f-499a-b0d1-3882c312dfb9', # Rainbow
+    'ded0d67a-eb71-4fe9-83c0-1d18b8ed497e', # Runaways
+    'bbd80354-597e-4d53-94e4-92b3a7cb8f2c' # Saxon    
+    ]
 
 aliceCooperPerson = 'ee58c59f-8e7f-4430-b8ca-236c4d3745ae'
 hollywoodVampires = 'd30a1d2b-e88d-4470-89cb-69d8c335ce3d'
