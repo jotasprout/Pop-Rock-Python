d7ba76076f9d0a93ca2867b4642fd46acbedfb35
jotasprout <jotasprout@gmail.com>
2019-03-22 14:17:09 -0400

    daily data

--- artistsData.py
+++ artistsData.py
@@ -26,79 +26,25 @@ mbid_array = [
     'bbd80354-597e-4d53-94e4-92b3a7cb8f2c' # Saxon    
     ] 
 
+# SPOTIFY IDs
+
 amboydukes_Spot = '5cVLuEqb7aOHuzwssXHzWI'
-anvil_Spot = ''
+anvil_Spot = '0maNhqGx60ucjS4UaAH6cP'
 blackSabbath_Spot = '5M52tdBnJaKSvOpJGz8mfZ'
 aliceCooper_Spot = '3EhbVgyfGd7HkpsagwL9GS'
 dio_Spot = '4CYeVo5iZbtYGBN4Isc3n6'
 elf_Spot = '3RYdggbT5C9r4BsljokJ1Q'
-evilStig_Spot = ''
+evilStig_Spot = '5NhjPre67qjeeQP4KHDHpe'
 HeavenHell_Spot = '4UjiBRkTw9VmvDZiJZKPJ7'
-hollywoodVampires_Spot = ''
-joanJett_Spot = ''
-meatLoaf_Spot = ''
+hollywoodVampires_Spot = '3k4YA0uPsWc2PuOQlJNpdH'
+joanJett_Spot = '1Fmb52lZ6Jv7FMWXXTPO3K'
+meatLoaf_Spot = '7dnB1wSxbYa8CejeVg98hz'
 metallica_Spot = '2ye2Wgw4gimLv2eAKyk1NB'
 motleyCrue_Spot = '0cc6vw3VN8YlIcvr1v7tBL'
 tednugent_Spot = '21ysNsPzHdqYN2fQ75ZswG'
 ozzyOsbourne_Spot = '6ZLTlhejhndI4Rh53vYhrY'
 queen_Spot = '1dfeR4HaWDbWqFHLkxsg1d'
-quietRiot_Spot = ''
+quietRiot_Spot = '1dLWg6m8RRhizsdqJbhyj3'
 rainbow_Spot = '6SLAMfhOi7UJI0fMztaK0m'
-runaways_Spot = ''
-saxon_Spot = ''
-
-aliceCooperPerson = 'ee58c59f-8e7f-4430-b8ca-236c4d3745ae'
-hollywoodVampires = 'd30a1d2b-e88d-4470-89cb-69d8c335ce3d'
-aliceCooperBand = '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd'
-
-acb14 = 'data/AliceCooper_BAND_1330pm_021419.json'
-acp14 = 'data/AliceCooper_PERSON_almost_11am_021419.json'
-acb15 = 'data/AliceCooperBand_021519.json'
-acp15 = 'data/AliceCooperPerson_021519.json'
-
-anvil = '07a85e96-bb72-4930-b41d-24853f4a4ede'
-anvildata = 'data/Anvil_021519_1404pm.json'
-
-blackSabbath = '5182c1d9-c7d2-4dad-afa0-ccfeada921a8'
-bs = 'data/BlackSabbath_021419_1640pm.json'
-
-dio = 'c55193fb-f5d2-4839-a263-4c044fca1456'
-diodata = 'data/Dio_021419_1714pm.json'
-dioAndtheProphets = '9f6c4063-ce0a-4b71-a7b7-a32c91997260'
-dioAndtheRedcaps = '883871a1-f154-4df8-a7f7-558ea456dd0a'
-electricElves = '30f9591a-778b-40dd-be8f-105589f9c998'
-elf = '57e0e9f3-24b5-46a6-be00-be793ca26e21'
-elfdata = 'data/Elf_021419_1654.json'
-heavenHell = '484a1d40-0fb9-4768-acff-b570cedaacb4'
-hhdata = 'data/HeavenHell_021419_1644pm.json'
-rainbow = 'e3cb4543-210f-499a-b0d1-3882c312dfb9'
-rainbowdata = 'data/Rainbow_021419_5pm.json'
-
-ozzyOsbourne = '8aa5b65a-5b3c-4029-92bf-47a544356934'
-
-defLeppard = '7249b899-8db8-43e7-9e6e-22f1e736024e'
-
-dickWagner = 'f92d6bfd-76e7-4394-aaec-9490756eb50c'
-frost = '6ab0acff-51de-45db-95cc-bdf5a6dd8578'
-
-joanJett = 'f376828a-b438-4fda-bb2e-dcd5fbe81f83'
-jjBlackhearts = '46e63d3b-d91b-4791-bb73-e9f638a45ea0'
-evilStig = '66bf7876-3898-47fa-8bdd-dc200f946cec'
-runaways = 'ded0d67a-eb71-4fe9-83c0-1d18b8ed497e'
-
-kidRock = 'ad0ecd8b-805e-406e-82cb-5b00c3a3a29e'
-
-meatLoaf = 'b134d1bf-c7c7-4427-93ac-9fdbc2b59ef1'
-stoneyMeatLoaf = '2cb3b264-277f-4d8f-bc86-1923ff8abdc0'
-popcornBlizzard = 'dad3fb79-469f-4892-bb39-56d01a9d2485'
-
-motleyCrue = '26f07661-e115-471d-a930-206f5c89d17c'
-
-queen = '0383dadf-2a4e-4d10-a46a-e9e041da8eb3'
-quietRiot = '5c6acb91-4b9b-4245-b92f-e817295c4ed0'
-qr = 'data/QuietRiot_021419_1651pm.json'
-
-tedNugent = 'e491fae8-3a5a-438e-8368-925753fb41a1'
-amboyDukes = '1a03f20c-26dd-4c26-bbe8-426e05ea46d5'
-
-saxon = 'bbd80354-597e-4d53-94e4-92b3a7cb8f2c'
+runaways_Spot = '5eTq3PxbOh5vgeRXKNqPyV'
+saxon_Spot = '71vVmHeNgCVSa5SVmfvscU'
