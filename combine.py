b54ced119cbcf0f2b0a6eae9a64f24c8df596811
jotasprout <jotasprout@gmail.com>
2019-03-25 14:26:45 -0400

    daily ish

--- combine.py
+++ combine.py
@@ -5,13 +5,13 @@ JJ = 'JoanJett'
 
 AC='AliceCooper'
 
-dataDate = '03-22-19'
+dataDate = '03-25-19'
 
 dataFolder = 'data/'
-fromArtistJSON = 'AliceCooper_Group_'
-toArtistJSON = 'AliceCooper_Person_'
-#fromArtistJSON = 'JoanJettandtheBlackhearts_Group_'
-#toArtistJSON = 'JoanJett_Person_'
+#fromArtistJSON = 'AliceCooper_Group_'
+#toArtistJSON = 'AliceCooper_Person_'
+fromArtistJSON = 'JoanJettandtheBlackhearts_Group_'
+toArtistJSON = 'JoanJett_Person_'
 ext = '.json'
 
 fromFilename = dataFolder + fromArtistJSON + dataDate + ext
