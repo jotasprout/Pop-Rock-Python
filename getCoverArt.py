47e4a111509409f595c708e8c151981803d7dbab
jotasprout <jotasprout@gmail.com>
2019-04-09 16:05:01 -0400

    coverArt archive script builder

--- getCoverArt.py
+++ getCoverArt.py
@@ -1,9 +1,9 @@
 docs = 'https://musicbrainz.org/doc/Cover_Art_Archive/API#.2Frelease.2F.7Bmbid.7D.2F.28.7Bid.7D.7Cfront.7Cback.29-.28250.7C500.7C1200.29'
 
-Children-of-the-Grave-album-mbid = "19772a1c-ea39-4104-bc99-ffc2203ea2ae"
+ChildrenoftheGravealbummbid = "19772a1c-ea39-4104-bc99-ffc2203ea2ae"
 # Vol 4 [1]
 
-Born-Again-Demos-album-mbid = "52c37691-e97e-3959-98c8-8b9a533eaeda"
+BornAgainDemosalbummbid = "52c37691-e97e-3959-98c8-8b9a533eaeda"
 # Born Again [1]
 
 coverArtArchiveURL = 'http://coverartarchive.org/release/'
@@ -13,3 +13,11 @@ release-mbid = ''
 coverArtURL = coverArtArchiveURL + release-mbid
 
 coverArtMB = ['images']['image']
+
+graveDemo = coverArtArchiveURL + ChildrenoftheGravealbummbid
+
+bornDemo = coverArtArchiveURL + BornAgainDemosalbummbid
+
+graveDemo2 = 'http://coverartarchive.org/release/19772a1c-ea39-4104-bc99-ffc2203ea2ae'
+
+bornDemo2 = 'http://coverartarchive.org/release/52c37691-e97e-3959-98c8-8b9a533eaeda'
