7f0af49b4cbe8b43735bb3435b9d08f3b1121c0d
jotasprout <jotasprout@gmail.com>
2019-04-09 13:28:05 -0400

    Start writing coverArtArchive mod

---
+++ getCoverArt.py
@@ -0,0 +1,15 @@
+docs = 'https://musicbrainz.org/doc/Cover_Art_Archive/API#.2Frelease.2F.7Bmbid.7D.2F.28.7Bid.7D.7Cfront.7Cback.29-.28250.7C500.7C1200.29'
+
+Children-of-the-Grave-album-mbid = "19772a1c-ea39-4104-bc99-ffc2203ea2ae"
+# Vol 4 [1]
+
+Born-Again-Demos-album-mbid = "52c37691-e97e-3959-98c8-8b9a533eaeda"
+# Born Again [1]
+
+coverArtArchiveURL = 'http://coverartarchive.org/release/'
+
+release-mbid = ''
+    
+coverArtURL = coverArtArchiveURL + release-mbid
+
+coverArtMB = ['images']['image']
