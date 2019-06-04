86d48353a5eef4876ab7b78ffa7a3d8142cddd24
JotaSprout <jotasprout@gmail.com>
2019-06-03 22:41:25 -0400

    Trying numPy

---
+++ numPyPlay.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+brady = np.array(['cindy', 'jan', 'marsha', 'bobby', 'peter', 'greg'])
+
+jackson = np.array(['jackie', 'jermaine', 'tito', 'marlon', 'randy', 'michael'])
+
+kiss = np.array(['peter', 'paul', 'ace', 'gene'])
+
+beatles = np.array(['george', 'john', 'paul', 'ringo'])
+
+sabbath = np.array(['ozzy', 'bill', 'tony', 'geezer'])
+
+zeppelin = np.array(['bonham', 'jones', 'plant', 'page'])
