cae9567b7ba680143831c0287d388fe6c0387263
JotaSprout <jotasprout@gmail.com>
2019-12-07 17:48:35 -0500

    Continue Rels script

--- crons/lastFM/musicBrainzArtistArtistRels.py
+++ crons/lastFM/musicBrainzArtistArtistRels.py
@@ -1,11 +1,20 @@
 import requests
 import json
 import pprint
-import lastFM
-import musicBrainz
-import artistsData
 
-mbid_rels_array = [
+# MusicBrainz variables
+MusicBrainz_artistArtistRelsPrefix = 'https://www.musicbrainz.org/ws/2/artist/'
+
+# Part of URL for getting MusicBrainz artist's artist relationships
+MusicBrainz_artistArtistRelsSuffix = '?inc=artist-rels&fmt=json'
+
+# Get just artist artist-rels from MusicBrainz
+
+def makeArtistRelsURL(MusicBrainz_artistMBID):
+    getArtist_totalURL = MusicBrainz_artistArtistRelsPrefix + MusicBrainz_artistMBID + MusicBrainz_artistArtistRelsSuffix
+    return getArtist_totalURL
+
+anchorArtists = [
     'ee58c59f-8e7f-4430-b8ca-236c4d3745ae', # Alice Cooper person   
     '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd', # Alice Cooper band 
     'fc4953aa-6bf4-4f1f-8e47-5ac79ca428e2', # Ronnie James Dio 
@@ -13,97 +22,66 @@ mbid_rels_array = [
     '79491354-3d83-40e3-9d8e-7592d58d790a' # deepPurple    
 ]
 
-artistRelsDicts = [
-    {
-        "name": "Alice Cooper",
-        "mbid": "ee58c59f-8e7f-4430-b8ca-236c4d3745ae",
-        "type": "Person",
-        "relations": []
-    },
-]
-
-artistsFrom = []
-artistsFrom = artistsRelsDicts
-artistsTo = []
-
-MusicBrainz_artistMBID = ''
-
-# MusicBrainz variables
-MusicBrainz_baseURL = 'https://www.musicbrainz.org/ws/2/'
-
-# Part of URL for using artist MBID
-MusicBrainz_artistMethod = 'artist/'
-
-# Part of URL for getting MusicBrainz artist's artist relationships
-MusicBrainz_artistArtistRels = '?inc=artist-rels'
-
-# MusicBrainz response format
-MusicBrainz_jsonFormat = '&fmt=json'
-
-# Get just artist artist-rels from MusicBrainz
-
-def makeArtistRelsURL(MusicBrainz_artistMBID):
-    getArtist_totalURL = MusicBrainz_baseURL + MusicBrainz_artistMethod + MusicBrainz_artistMBID + MusicBrainz_artistArtistRels + MusicBrainz_jsonFormat
-    return getArtist_totalURL
-
-BlackSabbathGroup = '5182c1d9-c7d2-4dad-afa0-ccfeada921a8'
-aliceCooperGroup = '4d7928cd-7ed2-4282-8c29-c0c9f966f1bd'
-aliceCooperPerson = 'ee58c59f-8e7f-4430-b8ca-236c4d3745ae'
-
 def get_artists_rels(artistVar):
-    
-    # Get artist relationships from MusicBrainz
 
     MusicBrainz_artistMBID = artistVar
 
-    getArtistRelsFromMusicBrainz = musicBrainz.makeArtistRelsURL(MusicBrainz_artistMBID)
+    getArtistRelsURL = makeArtistRelsURL(MusicBrainz_artistMBID)
 
-    ArtistRelsFromMB = requests.get(getArtistRelsFromMusicBrainz)
+    ArtistRelsFromMB = requests.get(getArtistRelsURL)
 
     artistRelsMB = json.loads(ArtistRelsFromMB.text)
 
-    # BUILD ARTIST DICTIONARY
+    # Make anchor artist
     artist = {}
-
     artist['name'] = artistRelsMB['name']
+    artistName = artist['name']
+    artist['mbid'] = artistRelsMB['id']
+    artist['type'] = artistRelsMB['type']
+    artistType = artist['type']
+    artist['relations'] = []
+    thisArtistRelations = artist['relations']
 
     # Get Relations for Artist from musicBrainz
-    artist['relations'] = []
     relations = artistRelsMB['relations']
-    #artist['relations'] = artistRelations
 
-    for rel in relations:
-        relTypeID = rel['type-id']
+    for relation in relations:
+        # Get only relation info that I want
+
+        relTypeID = relation['type-id']
         # Do not want Tribute bands
         # tribute = 'a6f62641-2f58-470e-b02b-88d7b984dc9f'
         member = '5be4c609-9afa-4ea0-910b-12ffb71e3821'
         if relTypeID == member:
             aRelation = {}
-            aRelation['type'] = rel['type']
-            aRelation['mbid'] = rel['artist']['id']
-            aRelation['name'] = rel['artist']['name']
-            aRelation['attributes'] = rel['attributes']
-            relations = relations + [aRelation]
+            aRelation['type'] = relation['type']
+            aRelation['mbid'] = relation['artist']['id']
+            aRelation['name'] = relation['artist']['name']
+            aRelation['attributes'] = relation['attributes']
+            #thisArtistRelations = thisArtistRelations + [aRelation]
+            thisArtistRelations.append(aRelation)
 
     print (artist)
 
-    # Add this artist to myArtists list
-    artistsTo.append(artist)
+    artistRelsJSON = json.dumps(artist, indent=4)
 
-putAllInArtistsTo()
+    # Write artist to file
+    artistNameFor_file_name = artistName.replace(' ', '')
+    artistTypeFor_file_name = artistType
 
-print(len(artistsTo))
+    absPathFor_file_name = '/home/roxorsox/public_html/poprock/data_text/'
 
-relsToday['myArtists'] = artistsTo
+    newFilename = absPathFor_file_name + artistNameFor_file_name + '_' + artistTypeFor_file_name  + '_Rels.json'
 
-artistsrelsJSON = json.dumps(relsToday, indent=4)
+    encodedFilename = newFilename.encode('utf-8')
 
-absPathForFileName = '/home/roxorsox/public_html/poprock/data_text/relsTest'
+    f = open (encodedFilename, 'w')
+    f.write (artistRelsJSON)
+    f.close()
 
-newFilename = absPathForFileName + dateFor_file_name + '.json'
+    print("File written with name " + newFilename)  
+    pprint.pprint(artist)
 
-f = open (newFilename, 'w')
-f.write (artistsrelsJSON)
-f.close()
-
-print("File written with name " + newFilename)  
+for anchorArtist in anchorArtists:
+    artistVar = anchorArtist
+    get_artists_rels(artistVar)
